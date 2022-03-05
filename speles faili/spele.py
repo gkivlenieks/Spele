2e80f0af9fccf6139bbb9d5c181f45830fb0da37
gkivlenieks <kivlenieksgustavs@gmail.com>
2022-03-05 09:00:25 +0200

    m

--- speles faili/spele.py
+++ speles faili/spele.py
@@ -22,7 +22,7 @@ MainCanvas = Canvas(
 MainCanvas.pack()
 #creation of cube
 cubee = MainCanvas.create_rectangle(xx, yy, xx + 20, yy - 20, fill='yellow')
-target = MainCanvas.create_rectangle(cx, cy, cx + 50, cy + 50, fill='brown')
+target = MainCanvas.create_rectangle(cx, cy, cx + 20, cy + 20, fill='brown')
 #creating za lines
 MainCanvas.create_line(0, 0, canvazwidth, 0, width=linewidth, fill=linecolor)
 MainCanvas.create_line(canvazwidth, 0, canvazwidth, canvazheight, width=linewidth, fill=linecolor)
@@ -48,12 +48,12 @@ def MoveCube(direction):
         xx -= 5
         MainCanvas.delete(cubee)
         cubee = MainCanvas.create_rectangle(xx, yy, xx + 20, yy - 20, fill='yellow')
-    if xx < cx + 50 and xx > cx and yy < cy + 50 and yy > cy:
+    if xx < cx + 20 and xx > cx and yy < cy + 20 and yy > cy:
         print("bruh") 
         MainCanvas.delete(target)
         cx = randrange(0, canvazwidth, 5)
         cy = randrange(0, canvazheight, 5)
-        target = MainCanvas.create_rectangle(cx, cy, cx + 50, cy + 50, fill='brown')
+        target = MainCanvas.create_rectangle(cx, cy, cx + 20, cy + 20, fill='brown')
 
 
 def on_keypress(event):
